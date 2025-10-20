7adf7b4dbf932f6b9146931f9bc54a688f712423
xscapefromme <markospineda585@gmail.com>
2025-10-19 20:56:27 -0700

    getting cards to active tab if checked
    filter() may be the way to go but will work on that tomorrow. Code getting messy and think thats going to be the easier way where i have allCards and just filter it based on event listeners instead of just individually pushing into an empty array.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const allBtn = document.getElementById("allBtn");
 const activeBtn = document.getElementById("activeBtn");
 const inactiveBtn = document.getElementById("inactiveBtn")
@@ -14,7 +15,7 @@ let inactiveCards = [];
 console.log(allCards);
 console.log([...activeCards])
 console.log(activeCards.length)
-
+console.log([...inactiveCards])
 async function loadData() {
   try {
     const response = await fetch("data.json");
@@ -34,11 +35,12 @@ async function loadData() {
 
       const labelForToggleBtn = document.createElement("label");
       labelForToggleBtn.classList.add("toggleBtn");
-      labelForToggleBtn.innerHTML = `<input type="checkbox"> <span class="slider"></span> `;
+      labelForToggleBtn.innerHTML = `<input class="toggleBtn" type="checkbox"> <span class="slider"></span> `;
 
       const divBottomContainer = document.createElement("div");
 
       divBottomContainer.classList.add("divBottomContainer");
+      
 
       divBottomContainer.append(removeBtn, labelForToggleBtn);
       card.append(divBottomContainer);
@@ -47,25 +49,39 @@ async function loadData() {
       list.append(card);
     });
 
-    const allToggleBtns = document.querySelectorAll('input')
+
+        const allToggleBtns = document.querySelectorAll('input')
+       
+
     
+
+    
+
+    const allCheckedToggle = document.querySelectorAll(
+    '.toggleBtn input:checked')
+
+    console.log(allCheckedToggle)
 
     allToggleBtns.forEach((btn) => {
         const btnIndex = [...allToggleBtns].indexOf(btn)
         console.log(btnIndex)
-        btn.addEventListener('change', () => {
+        btn.addEventListener('change', () => {  
             console.log('clicked')
             if(btn.checked){
                 const copied = allCards.slice(btnIndex, btnIndex + 1)
                 activeCards.push(copied[0])
-                console.log(activeCards.push(copied[0]))
-                  
+
+                console.log([...activeCards])
+            }else {
+            
+             inactiveCards.push(inactiveCopied)
+
             }
 
         })
     })
-        
 
+  
     
     
 
@@ -79,23 +95,39 @@ async function loadData() {
 
 
 
-allBtn.addEventListener('click', function() {
+allBtn.addEventListener('click', () => {
     allBtn.classList.add('active')
     activeBtn.classList.remove('active')
     inactiveBtn.classList.remove('active')
+
+   
+    list.append(...allCards)
+
 })
 
-activeBtn.addEventListener('click', function(){
+ 
+activeBtn.addEventListener('click', () => {
+    activeBtn.classList.add('active')
     allBtn.classList.remove('active')
     inactiveBtn.classList.remove('active')
-    activeBtn.classList.add('active')
-    allBtn.classList.remove('allBtn')
+
+    list.innerHTML = ''
+    list.append(...activeCards)
+
 })
 
-inactiveBtn.addEventListener('click', function(){
+ 
+
+
+
+inactiveBtn.addEventListener('click', () => {
     allBtn.classList.remove('active')
     activeBtn.classList.remove('active')
     inactiveBtn.classList.add('active')
+
+    list.innerHTML = ''
+    list.append(...inactiveCards)
+
 })
 
 
